9993905ac162d6e4ea9b71d76d2c8b7f71417d42
Ayooluwa Oyewo <ayooluwaoyewo@gmail.com>
2019-01-02 21:13:43 +0100

    adding file structure and updating package.json

--- app/server.js
+++ app/server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
+const cors = require('cors');
+
 const app = express();
+
+// enable cors in dev
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
-app.listen(8000, () => {
-  console.log('Example app listening on port 8000!');
+// mount server
+app.listen(config.port, config.host, () => {
+  console.log(`app running on http://${config.host}:${config.port}`);
+
 });
