dab91b27ee9850c39021e7127543d9dafe9e96d0
Ayooluwa Oyewo <ayooluwaoyewo@gmail.com>
2019-01-16 01:50:21 +0100

    edited the database management function

--- app/db/db.js
+++ app/db/db.js
@@ -3,11 +3,11 @@ import pool from './pool';
 
 
 const createTables = () => {
-  pool.query('CREATE TABLE IF NOT EXISTS users(user_id SERIAL PRIMARY KEY, firstname VARCHAR(100) NOT NULL, lastname VARCHAR(100) NOT NULL, othernames VARCHAR(100) NOT NULL, email VARCHAR(100) UNIQUE NOT NULL, username VARCHAR(50) NOT NULL, registered DATE NOT NULL, isAdmin BOOL DEFAULT(false))',
+  pool.query('CREATE TABLE IF NOT EXISTS users(user_id SERIAL PRIMARY KEY, firstname VARCHAR(100), lastname VARCHAR(100), othernames VARCHAR(100), email VARCHAR(100) UNIQUE NOT NULL, username VARCHAR(50) NOT NULL, registered DATE NOT NULL, isAdmin BOOL DEFAULT(false))',
     () => {
 
     });
-  pool.query('CREATE TABLE IF NOT EXISTS parcels(id SERIAL PRIMARY KEY, email VARCHAR(150) UNIQUE NOT NULL, password VARCHAR(150) NOT NULL)',
+  pool.query('CREATE TABLE IF NOT EXISTS parcels(parcel_id SERIAL PRIMARY KEY, placedBy INTEGER REFERENCES users(user_id), weight float NOT NULL, weightmetric VARCHAR(50) NOT NULL, sentOn DATE NOT NULL, deliveredOn DATE, status VARCHAR(50), from VARCHAR(500), to VARCHAR(500), currentLocation VARCHAR(500) )',
     () => {
     });
 };
